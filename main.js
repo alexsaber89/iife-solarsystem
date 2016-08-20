ffda5e74b08f1e03fbfe073865877155f08196f5
Alex Saber <Alex Saber>
2016-08-20 11:27:31 -0500

    added getters/setters to SolarSystem

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log(SolarSystem.lastModifiedDate);
+console.log(SolarSystem.setPlanets("Venus"));
+console.log(SolarSystem.getPlanets());
+console.log(SolarSystem.setActiveSpacecraft("Starship Enterprise"));
+console.log(SolarSystem.getActiveSpacecraft());
+console.log(SolarSystem.setPlanetsLandedOn(18));
+console.log(SolarSystem.getPlanetsLandedOn());
